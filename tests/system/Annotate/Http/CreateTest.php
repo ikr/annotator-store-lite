db76ef084493720f48f731990db2a5840f3f6138
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-05-22 19:05:29 +0200

    Start system/Annotate/Http/CreateTest

---
+++ tests/system/Annotate/Http/CreateTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Annotate\Http;
+
+use Guzzle\Http\Client;
+
+class CreateTest extends \PHPUnit_Framework_TestCase {
+    public function testCreatedAnnotationAppearsInTheInfdex() {
+        $resp = (new Client('http://127.0.0.1'))->post(
+            '/annotations',
+            null,
+            json_encode(self::newAnnotationData())
+        )->send();
+    }
+
+//--------------------------------------------------------------------------------------------------
+
+    private static function newAnnotationData() {
+        return [
+            'text' => 'Eyes, look your last. Arms, take your last embrace',
+            'more' => ', and lips, O you The doors of breath, seal with a righteous kiss. A dateless bargain to engrossing death.'
+        ];
+    }
+}
