46db7072f8f1d03a5ce040b045a8e8806d5a752f
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-05-26 17:37:40 +0200

    CONFIG creation

--- guest_os_install.sh
+++ guest_os_install.sh
@@ -8,6 +8,9 @@ sudo mv composer.phar /usr/local/bin/composer
 composer install --dev
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp $DIR/CONFIG.sample.json $DIR/CONFIG.json
+
 sqlite3 $DIR/data/db.sqlite ''
 php $DIR/scripts/init_db.php
 chmod 777 $DIR/data/db.sqlite
